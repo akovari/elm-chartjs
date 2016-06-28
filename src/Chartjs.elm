3df92974e3718d546f221961e292b77d5917fd35
Adam Kovari <kovariadam@gmail.com>
2016-06-28 13:38:26 +0200

    support for elm 0.17

--- src/Chartjs.elm
+++ src/Chartjs.elm
@@ -1,12 +1,16 @@
-module Chartjs where
+module Chartjs exposing (..)
 
 import Color exposing (Color)
-import Graphics.Element exposing (Element)
+import Element exposing (Element)
 import Json.Encode exposing (Value, encode)
 import Native.Chartjs
 
+
 showRGBA : Color -> String
-showRGBA = Native.Chartjs.showRGBA
+showRGBA =
+    Native.Chartjs.showRGBA
+
 
 chartRaw : Int -> Int -> Value -> Element
-chartRaw w h v = Native.Chartjs.chartRaw w h <| encode 0 v
+chartRaw w h v =
+    Native.Chartjs.chartRaw w h <| encode 0 v
